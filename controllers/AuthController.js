414cef535fdfe7b0b70daf047a20b68858c5d843
onkar-giram <meonkargiram@gmail.com>
2023-11-11 09:49:02 +0530

    Auth complete

---
+++ controllers/AuthController.js
@@ -0,0 +1,110 @@
+const User = require("../models/UserModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+exports.signup = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+
+    const existingUser = await User.findOne({ email: email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "user already exists",
+      });
+    }
+
+    //secure password
+    let hashedPassword;
+    try {
+      hashedPassword = await bcrypt.hash(password, 10);
+    } catch (err) {
+      res.status(500).json({
+        success: false,
+        message: `Error while hashing password: ${err}`,
+      });
+    }
+
+    //create entry for user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      role,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "User created successfully",
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: `Error while signing up: ${err}`,
+    });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Please fill all the details carefully",
+      });
+    }
+
+    //check for user
+    let user = await User.findOne({ email: email });
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "User Not Registered",
+      });
+    }
+
+    const payload = {
+      email: user.email,
+      id: user._id,
+      role: user.role,
+    };
+
+    //verify user
+    if (await bcrypt.compare(password, user.password)) {
+      //password match
+      let token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: "2h",
+      });
+      // user = user.toObject();
+      user.token = token;
+      user.password = undefined;
+
+      const options = {
+        expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+      };
+
+      res.cookie("token", token, options).status(200).json({
+        status: true,
+        token,
+        user,
+        message: "User logged in successfully",
+      });
+    } else {
+      //password not matched
+      return res.status(403).json({
+        success: false,
+        message: "Password incorrect",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: `Error while loggin up: ${err}`,
+    });
+  }
+};
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
+const { login, signup } = require("./routes/AuthRoute");
 
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 
+const authRoute = require("./routes/AuthRoute");
+
 app.use(express.json());
+
+app.use("/api/v1/auth", authRoute);
 
 app.listen(PORT, () => {
   console.log(`Server up and running on port: ${PORT}`);
